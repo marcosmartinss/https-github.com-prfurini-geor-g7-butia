9ac241ef1bae85415eacb5967ed4201158ee9d09
npasqualotto <pasqualotto@alumni.usp.br>
2021-11-03 14:41:21 -0300

    dados filtrados para galia e garça

--- script_npasqualotto.r
+++ script_npasqualotto.r
@@ -1,8 +1,3 @@
-#' ---
-#' titulo:  
-#' autores: 
-#' data: 2021-11-03
-#' ---
 
 # packages ----------------------------------------------------------------
 library(tidyverse)
@@ -10,16 +5,27 @@ library(sp)
 library(sf)
 
 # Lendo os dados de occorrencia das spp ------------------------------------------------------------------
-atlantic.birds <- read.csv("ATLANTIC_BIRDS_quantitative.csv")
-head(atlantic.birds)
-dim(atlantic.birds)
-str(atlantic.birds)
+# atlantic.birds <- readr::read_csv(
+#   here::here("ATLANTIC_BIRDS_quantitative.csv"))
+atlantic.birds <- read.csv(
+  here::here("ATLANTIC_BIRDS_quantitative.csv"))
 
-
-# filtrar linhas por valores de uma coluna
-atlantic.birds <- atlantic.birds %>% 
-  dplyr::filter(Municipality == "GÁLIA")
 head(atlantic.birds)
 dim(atlantic.birds)
 
+
+# filtrar dados para os municipios de Gallia por valores de uma coluna
 sort(unique(atlantic.birds$Municipality))
+
+atlantic.birds.filtro <- atlantic.birds %>% 
+  dplyr::filter(Municipality %in% c("GÁLIA", "GAR+çA", "GARCA"))
+head(atlantic.birds.filtro)
+dim(atlantic.birds.filtro)
+
+# criando objeto espacial com o pacote sf 
+atlantic.birds.vct <- atlantic.birds.filtro %>% 
+  sf::st_as_sf(coords = c("Longitude_x", "Latitude_y"), crs = 4326)
+atlantic.birds.vct
+
+# plot
+plot(atlantic.birds.vct$geometry, pch = 20, main = NA, axes = TRUE, graticule = TRUE)
