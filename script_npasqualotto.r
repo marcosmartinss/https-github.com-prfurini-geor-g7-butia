aecc4288648a10a0b616897239597c9478507089
npasqualotto <pasqualotto@alumni.usp.br>
2021-11-03 15:39:29 -0300

    dados filtrados para estado SP

--- script_npasqualotto.r
+++ script_npasqualotto.r
@@ -13,20 +13,31 @@ atlantic.birds <- read.csv(
 head(atlantic.birds)
 dim(atlantic.birds)
 
-
-# filtrar dados para os municipios de Gallia por valores de uma coluna
-sort(unique(atlantic.birds$Municipality))
-
-atlantic.birds.filtro <- atlantic.birds %>% 
-  dplyr::filter(Municipality %in% c("GÁLIA", "GAR+çA", "GARCA"))
-head(atlantic.birds.filtro)
-dim(atlantic.birds.filtro)
-
 # criando objeto espacial com o pacote sf 
-atlantic.birds.vct <- atlantic.birds.filtro %>% 
+atlantic.birds.points <- atlantic.birds %>% 
   sf::st_as_sf(coords = c("Longitude_x", "Latitude_y"), crs = 4326)
-atlantic.birds.vct
+atlantic.birds.points
 
-# plot
-plot(atlantic.birds.vct$geometry, pch = 20, main = NA, axes = TRUE, graticule = TRUE)
+# baixar shape Brasil 2020
+br_2020 <- geobr::read_country(year = 2020)
+
+# filtrar apenas o estado de Sao Paulo
+sp <- br_2020 %>% 
+  dplyr::filter(abbrev_state == "SP")
+head(sp)
+
+# filtrar dados para o estado de SP
+atlantic.birds.sp <- atlantic.birds.points %>% 
+  dplyr::filter(State == "SP")
+head(atlantic.birds.sp)
+dim(atlantic.birds.sp)
+
+# numero de pontos de ocorrencia diferentes
+length(unique(atlantic.birds.sp$geometry)) # Sao 349!
+
+# plot dados de aves em Sao Paulo
+plot(sp$geom, pch = 20, main = NA, axes = TRUE, graticule = TRUE)
+plot(atlantic.birds.sp$geometry, pch = 20, main = NA, axes = TRUE, graticule = TRUE, add= TRUE)
+
+
 
