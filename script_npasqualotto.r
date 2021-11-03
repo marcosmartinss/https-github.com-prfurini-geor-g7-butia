d8cef0523cf8fef7a38f30dfdd01533648f144b0
npasqualotto <pasqualotto@alumni.usp.br>
2021-11-03 14:45:51 -0300

    _

--- script_npasqualotto.r
+++ script_npasqualotto.r
@@ -17,6 +17,7 @@ dim(atlantic.birds)
 # filtrar dados para os municipios de Gallia por valores de uma coluna
 sort(unique(atlantic.birds$Municipality))
 
+<<<<<<< HEAD
 atlantic.birds.filtro <- atlantic.birds %>% 
   dplyr::filter(Municipality %in% c("GÁLIA", "GAR+çA", "GARCA"))
 head(atlantic.birds.filtro)
@@ -29,3 +30,5 @@ atlantic.birds.vct
 
 # plot
 plot(atlantic.birds.vct$geometry, pch = 20, main = NA, axes = TRUE, graticule = TRUE)
+=======
+>>>>>>> 620521f25a43c0ada1e188a6356d03a635125c89
