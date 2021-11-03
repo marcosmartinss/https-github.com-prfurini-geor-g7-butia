3959b8cde45721bc8184fcf05c8e180742938ffa
fulbertgnonlonfoun01e <fulbertgnonlonfoun@gmail.com>
2021-11-03 12:00:05 -0300

    script_fulbert.r

--- script.r
+++ script.r
@@ -1,3 +1,8 @@
+#' ---
+#' title: tabalho_fina_disciplina - estrutura e manipulacao de dados na linguagem R
+#' author: Fulbert Gnonlonfoun
+#' date: 2021-11-03
+#' ---
 atlantic.birds <- read.csv("ATLANTIC_BIRDS_quantitative.csv")
 head(atlantic.birds)
 
