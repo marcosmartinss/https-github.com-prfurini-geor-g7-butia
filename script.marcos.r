efea69b4471cae4ff6f54b959a35ddd82708c1a2
marcosmartinss <bioturismologo@gmail.com>
2021-11-04 10:14:31 -0300

    ucs

--- script.marcos.r
+++ script.marcos.r
@@ -1,5 +1,9 @@
+library(tidyverse)
+library(sp)
 library(sf)
-
+#------------------------------------------------------------------------------------------------------------------------
+  
+# Camada de UCs Estaduais de Protecao Integral
 uc <- sf::st_read(here::here("ucsei.shp"), quiet = TRUE)
 uc
 
@@ -7,24 +11,32 @@ plot(uc [10], col = c("blue", "orange", "gray30", "forestgreen", "green"), main 
 
 sf::st_crs(uc)
 
+# Criando um diretorio
 dir.create(here::here("vetor"))
 
-for(i in c(".dbf", ".prj", ".shp", ".shx")){
-  download.file(url = paste0("http://datageo.ambiente.sp.gov.br/geoserver/ows?service=WMS&version=1.1.0&request=GetMap&layers=datageowms:UCS_ESTADUAIS_SP_POL&styles=&bbox=-51.81640316514246,-24.43011526988209,-44.32236139998661,-20.980665679200712&width=287&height=245&tiled=true&srs=EPSG:4326&format=image/png&transparent=true", i),
-                destfile = here::here("vetor", paste0("ms_tmp_ucsfi", i), mode = "wb")
-  
+# Mapa de Galia_SP
 galia_uso <- sf::st_read(here::here("Galia_USO", "USO", "SP_3516606_USO.shp"), quiet = TRUE)
 galia_uso
 
 plot(galia_uso [5], col = c("blue", "orange", "gray30", "forestgreen", "green"), main = NA, axes = TRUE, graticule = TRUE)
 
+# Mapa Brasil
 br_2020 <- geobr::read_country(year = 2020, showProgress = FALSE)
 br_2020
 
 plot(br_2020$geom, col = "gray", main = NA, axes = TRUE, graticule = TRUE)
 
-git add -Av
+# filtrar apenas o estado de Sao Paulo
+sp <- br_2020 %>% 
+  dplyr::filter(abbrev_state == "SP")
+head(sp)
 
-UCS_FED_kml <- sf::st_read(here::here("C:/Users/profm/Documents/Doutorado Geografia PUC Minas/Disciplinas 2021_02/Geospacial com R/GitHub/projeto_final/https-github.com-prfurini-geor-g7-butia", "datageowms-UC_Federais_MMA.kml"), quiet = TRUE)
-UCS_FED_kml
-                
+plot(sp$geom, col = "gray", main = NA, axes = TRUE, graticule = TRUE)
+
+# importando dados
+options(timeout = 600)
+
+for(i in c(".dbf", ".prj", ".shp", ".shx")){
+  download.file(url = paste0("https://geo.fbds.org.br/SP/AGUAI/USO/", i),
+                destfile = here::here("C:/Users/profm/Documents/Doutorado Geografia PUC Minas/Disciplinas 2021_02/Geospacial com R/GitHub/projeto_final/https-github.com-prfurini-geor-g7-butia", paste0("SP_3500303_USO", i)), mode = "wb")
+}
